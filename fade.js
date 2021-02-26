cb17f00f5a4ae7132d83f421045845dae15fbddf
KamataOrchestra <75162517+KamataOrchestra@users.noreply.github.com>
2021-02-26 19:55:36 +0900

    Add files via upload

---
+++ fade.js
@@ -0,0 +1,36 @@
+ScrollReveal().reveal('.main-visual',{
+    duration: 2000,
+    delay:300,
+});
+ScrollReveal().reveal('.information',{
+    duration: 2000,
+    delay:300,
+});
+ScrollReveal().reveal('.join',{
+    duration: 2000,
+    delay:300,
+});
+ScrollReveal().reveal('.goods',{
+    duration: 1000,
+    delay:300,
+    origin:'left',
+    distance: '200px',
+});
+ScrollReveal().reveal('.kuri',{
+    duration: 1000,
+    delay:300,
+    origin:'right',
+    distance: '200px',
+});
+ScrollReveal().reveal('.main-header',{
+    duration: 1000,
+    delay:600,
+    origin:'top',
+    distance: '500px',
+});
+ScrollReveal().reveal('.category-header',{
+    duration: 1000,
+    delay:600,
+    origin:'top',
+    distance: '500px',
+});
